35d732af99e4e5877ab884643a5c9771327fc6cd
navnitsingh0110 <ns011125@gmail.com>
2025-01-14 19:15:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let targetDiv = document.getElementById("targetDiv");
+
+targetDiv.style.backgroundColor = "yellow";
+targetDiv.style.margin = "20px";
+targetDiv.style.padding = "20px";
+targetDiv.style.fontSize = "18px";
+targetDiv.style.fontWeight = "bold";
+targetDiv.style.height = "200px";
+targetDiv.style.width = "300px";
+targetDiv.style.color = "blue";
